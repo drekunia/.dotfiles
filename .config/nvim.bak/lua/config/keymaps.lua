61df9a12761f85a0b269c7da56aeb2418afd6162
Andre Kurniawan <drekunia@gmail.com>
2024-11-11 01:17:10 +0700

    backup lazyvim

---
+++ .config/nvim.bak/lua/config/keymaps.lua
@@ -0,0 +1,84 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Global leader key
+vim.g.mapleader = " "
+
+-- Scrolls down or up by half a page and centers the cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Moves to the next or previous search result and centers the cursor
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Disable the `Q` key
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Navigate through quickfix and location lists and center the cursor
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
+
+-- Select all text in file
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Moves to the EOL with `E` and BOL with `B`
+vim.keymap.set("n", "E", "$", { noremap = false })
+vim.keymap.set("n", "B", "^", { noremap = false })
+
+-- Split windows with `ss` and `sv` (horizontal and vertical)
+vim.keymap.set("n", "ss", "<C-w>v")
+vim.keymap.set("n", "sv", "<C-w>s")
+
+-- Move between windows with `sh` and `sk` (left and right)
+vim.keymap.set("n", "sh", "<C-w>h")
+vim.keymap.set("n", "sk", "<C-w>k")
+vim.keymap.set("n", "sj", "<C-w>j")
+vim.keymap.set("n", "sl", "<C-w>l")
+
+-- Resize windows with `<C-w>`
+vim.keymap.set("n", "<C-w><left>", "<C-w><")
+vim.keymap.set("n", "<C-w><right>", "<C-w>>")
+vim.keymap.set("n", "<C-w><up>", "<C-w>+")
+vim.keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Move selected text up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Maps `<C-c>` to `ESC` in insert mode
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- Maps `jj`, `kk`, and `jk` to `ESC` in insert mode
+vim.keymap.set("i", "jj", "<Esc>", { noremap = false })
+vim.keymap.set("i", "kk", "<Esc>", { noremap = false })
+vim.keymap.set("i", "jk", "<Esc>", { noremap = false })
+
+-- Copies text to the system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>Y", [["+Y]])
+
+-- Delete and change a character without copying it
+vim.keymap.set({ "n", "v" }, "x", '"_x')
+
+-- Pastes from 0 register using `<leader>p` and `<leader>P`
+vim.keymap.set({ "n", "v" }, "<leader>p", '"0p')
+vim.keymap.set({ "n", "v" }, "<leader>P", '"0P')
+
+-- Change text without affecting registers
+vim.keymap.set({ "n", "v" }, "<leader>c", '"_c')
+vim.keymap.set({ "n", "v" }, "<leader>C", '"_C')
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
+vim.keymap.set({ "n", "v" }, "<leader>D", '"_D')
+
+-- Tab management
+-- vim.keymap.set("n", "te", ":tabedit<Return>")
+vim.keymap.set("n", "<tab>", ":tabnext<Return>", { noremap = true, silent = true })
+vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", { noremap = true, silent = true })
+
+-- Keymaps for dealing with word wrapping
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
